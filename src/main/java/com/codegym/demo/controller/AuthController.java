cd612b087740b1e91f83b29f165116164e51d427
LAPTOP-UA1EKRC9\HP <huynamvn93@gmail.com>
2021-10-10 22:52:10 +0700

    ban mới

--- src/main/java/com/codegym/demo/controller/AuthController.java
+++ src/main/java/com/codegym/demo/controller/AuthController.java
@@ -19,17 +19,15 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashSet;
 import java.util.Set;
 
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping
+@RequestMapping("")
 public class AuthController {
 
     @Autowired
